3818a38fbe4834cf2e00b2251cc6b0366f6f6a2e
Sally Lee <lsylcy0307@gmail.com>
2024-04-11 00:43:13 -0400

    changed prompt

--- lippinbott/src/pages/api/openai.ts
+++ lippinbott/src/pages/api/openai.ts
@@ -20,11 +20,13 @@ export default async function handler(
     return res.status(400).json({ error: "Prompt missing" });
   }
 
-  console.log(prompt);
+  const userPrompt = "Help me find resources about Mississippi valley."
+  const resource = "Catalog Record: Foot-prints of vanished races in the... | HathiTrust Digital Library Skip to main Skip to similar items Foot-prints of vanished races in the Mississippi valley : being an account of some of the monuments and relics of pre-historic races scattered over its surface, with suggestions as to their origin and uses / by A. J. Conant. Description Tools Cite this Export citation file Main Author Conant, A. J. (Alban Jasper), 1821-1915 Language(s) English Published St. Louis : C.R. Barns, 1879. Subjects Mound-builders > Mound-builders / Mississippi River Valley. Mississippi River Valley > Mississippi River Valley / Antiquities. Note Previously published in William Switzler's Illustrated history of Missouri (St. Louis, 1879) under title: Archaeology. The mounds and their builders. Physical Description x, 122 p. : ill. ; 27 cm. Locate a Print Version Find in a library Viewability Item Link Original Source Full view Harvard University Full view Harvard University Full view Cornell University Full view University of Illinois at Urbana-Champaign Full view New York Public Library Full view University of California View HathiTrust MARC record Similar Items Annual report Author New Jersey. Bureau of Industrial Statistics. Published 1878 Cigar Makers' Official Journal Author Cigar Markers' International Union of America. Published 1875 Dental record; a monthly journal of dental science art and literature Author Odonto-Chirurgical Society of Scotland. Published 1881 Transactions Author Norfolk and Norwich Naturalists' Society, Norwick Eng. Published 1874 The Medical world Published 1883 The Medical brief. A monthly journal of scientific medicine and surgery Published 1875 The Medical chronicle; a monthly record of the progress of medical schince Author Owens College. Medical Dept. Published 1884 Indian notes and queries Published 1883 Proceedings Author International Association of Officials of Bureaus of Labor, Factory Inspection and Industrial Commissions. Published 1883 The Organon Author Lippe, Adolph, 1812-1888. Published 1878";
+
   try {
     const chatCompletion = await openai.chat.completions.create({
       model: "gpt-4-turbo",
-      messages: [{"role": "user", "content": "Help me find resources related to entrepreneurship"}],
+      messages: [{"role": "user", "content": `I want you to ${userPrompt}. Give me a summary of the following page and how might it help with what I need: ${resource}`}], //summary for resource
     });
     
     const resp = chatCompletion.choices[0].message;
