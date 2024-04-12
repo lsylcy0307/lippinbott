182d4a8b0cfc44ea2684f850ce14e9f786e81a38
Sally Lee <lsylcy0307@gmail.com>
2024-04-12 00:27:20 -0400

    almost done

--- lippinbott/src/pages/Chatbot.tsx
+++ lippinbott/src/pages/Chatbot.tsx
@@ -9,7 +9,7 @@ type Resource = {
 };
 
 const ResourceCard: React.FC<Resource> = ({ title, description, link, recommendation, next_queries }) => {
-  const queries = next_queries.split(/[\.,?]/).map(query => query.trim()).filter(query => query);
+  const queries = next_queries.split(/[\.,;?]/).map(query => query.trim()).filter(query => query);
   return (
     <div className="w-full max-auto bg-white rounded-xl shadow-md overflow-hidden my-4">
       <div className="p-8">
@@ -38,12 +38,14 @@ const ResourceCard: React.FC<Resource> = ({ title, description, link, recommenda
 
 const Chatbot: React.FC = () => {
   const [input, setInput] = useState('');
+  const [userInput, setUserInput] = useState('');
   const [response, setResponse] = useState('');
   const [responseJson, setResponseJson] = useState(null);
   const [loading, setLoading] = useState(false);
 
   const handleSend = async () => {
     setLoading(true);
+    setUserInput(input);
     setInput('');
 
     try {
@@ -117,10 +119,11 @@ const Chatbot: React.FC = () => {
           </div>
         ) : responseJson && (
           <div>
-            {/* <div className="border border-gray-300 rounded p-4 bg-white shadow-lg w-full">
+            {/* <div className="border border-white-300 rounded p-4 bg-white shadow-lg w-full">
               <p className="font-normal text-gray-700"><strong>Lippinbott:</strong> {response}</p>
             </div> */}
-            <h2 className="text-3xl mt-5 text-white ">We found for you:</h2>
+            <h2 className="text-2xl mt-5 text-slate-300	">Your search: {userInput}</h2>
+            <h2 className="text-3xl mt-5 text-white	 ">We found for you:</h2>
             <div className="flex flex-wrap justify-center items-center">
               {(responseJson! as Resource[]).map((resource: Resource, index: number) => (
                 <ResourceCard key={index} {...resource} />
